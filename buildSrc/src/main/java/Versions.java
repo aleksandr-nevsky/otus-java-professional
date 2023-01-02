d3f873fb2240fdf4ef81d7b3501807cfab743a1e
Aleksandr Vvedensky <me@nevsky.cc>
2023-01-02 18:26:29 +0300

    Add L25-webServer.

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -5,4 +5,7 @@ public interface Versions {
     String guava = "31.1-jre";
     String asm = "9.2";
     String glassfishJson = "2.0.1";
+    String gson = "2.9.1";
+    String jetty = "11.0.7";
+    String freemarker = "2.3.31";
 }
