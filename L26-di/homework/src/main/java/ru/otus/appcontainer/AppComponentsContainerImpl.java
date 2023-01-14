ac1c4cdb2904431721d534c4ea1e1058a33969d4
Aleksandr Vvedensky <me@nevsky.cc>
2023-01-14 17:40:00 +0300

    L26-di. Complete hw.

--- L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L26-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,9 +1,17 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
@@ -17,6 +25,42 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
         // You code here...
+        try {
+            Object classInstance = configClass.getConstructor().newInstance();
+            Method[] methods = Arrays.stream(configClass.getMethods())
+                    .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                    .sorted(Comparator.comparingInt(o -> o.getAnnotation(AppComponent.class).order()))
+                    .toArray(Method[]::new);
+
+            for (Method method : methods) {
+                AppComponent appComponent = method.getAnnotation(AppComponent.class);
+                Object invokedMethod = callMethod(classInstance, method);
+                if (getAppComponent(appComponent.name()) != null) {
+                    throw new RuntimeException(appComponent.name() + " is present");
+                }
+
+                appComponentsByName.put(appComponent.name(), invokedMethod);
+                appComponents.add(invokedMethod);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object callMethod(Object classInstance, Method method) {
+        try {
+            method.setAccessible(true);
+            Parameter[] params = method.getParameters();
+            Object[] paramList = new Object[params.length];
+
+            for (int i = 0; i < params.length; i++) {
+                paramList[i] = getAppComponent(params[i].getType());
+            }
+
+            return method.invoke(classInstance, paramList);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +71,16 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        List<Object> filteredList = appComponents.stream().filter(p -> componentClass.isAssignableFrom(p.getClass())).toList();
+        if (filteredList.size() != 1) {
+            throw new RuntimeException("Incorrect component count.");
+        }
+
+        return (C) filteredList.get(0);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
