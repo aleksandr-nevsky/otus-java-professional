96fb5fb101a03fdc757844069e3f3a342484a978
Aleksandr Vvedensky <me@nevsky.cc>
2023-02-04 11:51:04 +0300

    Add hw32-concurrentCollections.

--- L32-concurrentCollections/QueueDemo/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ L32-concurrentCollections/QueueDemo/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -2,16 +2,21 @@ package ru.otus.services.processors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
 
-// Этот класс нужно реализовать
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final ConcurrentSkipListSet<SensorData> dataBuffer = new ConcurrentSkipListSet<>(Comparator.comparing(SensorData::getMeasurementTime));
+    private final ReentrantReadWriteLock dataBufferLock = new ReentrantReadWriteLock();
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
@@ -20,18 +25,32 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     @Override
     public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
+        int currentSize;
+
+        dataBufferLock.writeLock().lock();
+
+        dataBuffer.add(data);
+        currentSize = dataBuffer.size();
+
+        dataBufferLock.writeLock().unlock();
+
+        if (currentSize >= bufferSize) {
             flush();
         }
-    */
     }
 
     public void flush() {
+        dataBufferLock.writeLock().lock();
         try {
-            //writer.writeBufferedData(bufferedData);
+            if (!dataBuffer.isEmpty()) {
+                writer.writeBufferedData(dataBuffer.stream().toList());
+                dataBuffer.clear();
+            }
+
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
+        } finally {
+            dataBufferLock.writeLock().unlock();
         }
     }
 
